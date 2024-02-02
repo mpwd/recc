b33ce900dd02ac7f4fdfb50c47df947bc20e2f20
Dominic German <dominicgerman@gmail.com>
2024-02-01 19:39:39 -0600

    updated footer

--- footer.php
+++ footer.php
@@ -3,79 +3,82 @@ $todaysYear = date('Y');
 ?>
 <!----------- FOOTER ------------>
 <footer class="bg-primary text-white px-[10vw] py-8">
-    <div class="lg:grid grid-flow-col auto-cols-auto">
-        <div class="footer--contact">
-            <h2 class="mt-16 mb-6">Contact Us</h2>
-            <ul class="text-xl">
-                <li class="flex gap-4 my-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
-                        <circle opacity="0.1" cx="18" cy="18" r="18" fill="white" />
-                        <path d="M18 28C17.85 28 17.7 27.9301 17.61 27.8101C17.55 27.7401 16.19 26.0001 14.81 23.4301C14 21.9201 13.35 20.43 12.88 19.01C12.29 17.22 12 15.54 12 14C12 10.69 14.69 8 18 8C21.31 8 24 10.69 24 14C24 15.54 23.7 17.22 23.12 19.01C22.65 20.43 22.01 21.9101 21.19 23.4301C19.81 26.0001 18.45 27.7401 18.39 27.8101C18.29 27.9301 18.15 28 18 28ZM18 9C15.24 9 13 11.24 13 14C13 17.25 14.46 20.66 15.69 22.95C16.59 24.63 17.5 25.96 18 26.66C18.5 25.96 19.41 24.6201 20.32 22.9301C21.54 20.6501 23 17.24 23 14C23 11.24 20.76 9 18 9Z" fill="white" />
-                        <path d="M18 17C16.35 17 15 15.65 15 14C15 12.35 16.35 11 18 11C19.65 11 21 12.35 21 14C21 15.65 19.65 17 18 17ZM18 12C16.9 12 16 12.9 16 14C16 15.1 16.9 16 18 16C19.1 16 20 15.1 20 14C20 12.9 19.1 12 18 12Z" fill="white" />
-                    </svg>
-                    <a href="https://maps.app.goo.gl/wAtfvdJAcTVhbENc8" target="_blank" class="max-w-[21ch]">4900 North Damen Ave. Chicago, IL 60625</a>
-                </li>
-                <li class="flex gap-4 my-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
-                        <circle opacity="0.1" cx="18" cy="18" r="18" fill="white" />
-                        <path d="M22.98 29C21.21 29 19.33 28.5 17.38 27.51C15.59 26.6 13.82 25.29 12.26 23.73C10.7 22.17 9.4 20.4 8.49 18.6C7.5 16.65 7 14.77 7 13C7 11.85 8.07 10.7399 8.53 10.3199C9.19 9.70995 10.23 9 10.99 9C11.37 9 11.81 9.25002 12.38 9.77002C12.81 10.16 13.28 10.7001 13.76 11.3101C14.05 11.6801 15.49 13.58 15.49 14.49C15.49 15.24 14.64 15.7601 13.75 16.3101C13.4 16.5201 13.05 16.74 12.79 16.95C12.51 17.17 12.47 17.2901 12.46 17.3101C13.41 19.6801 16.31 22.58 18.68 23.52C18.7 23.52 18.82 23.4699 19.04 23.1899C19.25 22.9299 19.47 22.57 19.68 22.23C20.23 21.34 20.75 20.49 21.5 20.49C22.41 20.49 24.31 21.93 24.68 22.22C25.3 22.7 25.83 23.18 26.22 23.6C26.75 24.17 26.99 24.61 26.99 24.99C26.99 25.75 26.28 26.79 25.67 27.45C25.25 27.91 24.14 28.99 22.99 28.99L22.98 29ZM10.97 10C10.7 10 9.98 10.33 9.2 11.05C8.46 11.74 7.99 12.48 7.99 13C7.99 19.73 16.25 28 22.98 28C23.49 28 24.24 27.54 24.92 26.79C25.64 26 25.97 25.28 25.98 25.01C25.95 24.82 25.42 24.08 23.98 22.97C22.74 22.02 21.74 21.51 21.48 21.5C21.46 21.5 21.35 21.55 21.12 21.84C20.92 22.09 20.71 22.43 20.51 22.77C19.95 23.68 19.42 24.54 18.65 24.54C18.53 24.54 18.4 24.52 18.29 24.47C15.67 23.42 12.56 20.3199 11.51 17.6899C11.38 17.3799 11.36 16.8799 11.98 16.3199C12.31 16.0199 12.77 15.74 13.21 15.47C13.54 15.27 13.89 15.06 14.14 14.86C14.43 14.63 14.47 14.52 14.48 14.5C14.48 14.24 13.96 13.24 13.01 12C11.9 10.56 11.16 10.04 10.97 10Z" fill="white" />
-                        <path d="M26.48 18C26.2 18 25.98 17.78 25.98 17.5C25.98 13.36 22.62 10 18.48 10C18.2 10 17.98 9.78 17.98 9.5C17.98 9.22 18.2 9 18.48 9C20.75 9 22.88 9.87999 24.49 11.49C26.1 13.1 26.98 15.23 26.98 17.5C26.98 17.78 26.76 18 26.48 18Z" fill="white" />
-                        <path d="M23.48 18C23.2 18 22.98 17.78 22.98 17.5C22.98 15.02 20.96 13 18.48 13C18.2 13 17.98 12.78 17.98 12.5C17.98 12.22 18.2 12 18.48 12C21.51 12 23.98 14.47 23.98 17.5C23.98 17.78 23.76 18 23.48 18Z" fill="white" />
-                        <path d="M20.48 18C20.2 18 19.98 17.78 19.98 17.5C19.98 16.67 19.31 16 18.48 16C18.2 16 17.98 15.78 17.98 15.5C17.98 15.22 18.2 15 18.48 15C19.86 15 20.98 16.12 20.98 17.5C20.98 17.78 20.76 18 20.48 18Z" fill="white" />
-                    </svg>
-                    <a href="tel:7737847091" target="_blank">(773) 784-7091</a>
-                </li>
-                <li class="flex gap-4 my-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
-                        <circle opacity="0.1" cx="18" cy="18" r="18" fill="white" />
-                        <path d="M25.84 10.1267C25.7 9.99675 25.49 9.96675 25.31 10.0368L6.31 18.0368C6.12 18.1168 6 18.3067 6 18.5167C6 18.7267 6.14003 18.9068 6.34003 18.9768L12 20.8667V26.5068C12 26.7168 12.13 26.9068 12.33 26.9768C12.38 26.9968 12.44 27.0068 12.5 27.0068C12.65 27.0068 12.79 26.9367 12.89 26.8267L15.65 23.4267L20.15 27.8667C20.24 27.9567 20.37 28.0068 20.5 28.0068C20.54 28.0068 20.58 28.0067 20.62 27.9867C20.79 27.9467 20.92 27.8167 20.97 27.6467L25.97 10.6467C26.03 10.4667 25.97 10.2668 25.82 10.1368L25.84 10.1267ZM22.14 12.4567L12.42 19.9367L7.92999 18.4367L22.14 12.4567ZM13.01 20.7467L22.42 13.5068L15.23 22.3567L13.01 25.0867V20.7367V20.7467ZM20.27 26.5567L16.3 22.6368L24.33 12.7467L20.27 26.5567Z" fill="white" />
-                    </svg>
-                    <a href="mailto:office@ravenswoodcov.org" target="_blank">office@ravenswoodcov.org</a>
-                </li>
-            </ul>
-        </div>
-        <div class="footer--links">
-            <h2 class="mt-16 mb-6">
-                Links
-            </h2>
-            <ul class="text-xl">
-                <li class="my-4">
-                    <a href="<?php echo site_url('/about-us'); ?>">What we believe</a>
-                </li>
-                <li class="my-4">
-                    <a href="<?php echo site_url('/events'); ?>">Church events</a>
-                </li>
-                <li class="my-4">
-                    <a href="https://covchurch.org/" target="_blank">Our denomination</a>
-                </li>
-                <li class="my-4">
-                    <a href="https://centralconf.org/" target="_blank">ECC Central Conference</a>
-                </li>
-            </ul>
-        </div>
-        <div class="footer--socials">
-            <h2 class="mt-16 mb-6">Social media</h2>
-            <ul class="flex gap-6">
-                <li>
-                    <a href="https://www.facebook.com/ravenscov/" target="_blank">
-                        <img src="<?php echo get_theme_file_uri('/images/facebook.svg') ?>" class="" alt="facebook">
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.youtube.com/channel/UCGUlqyTaZxBPs-oJGn2nxvQ" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="53" height="53" viewBox="0 0 53 53" fill="none">
-                            <rect x="12" y="16" width="29.6471" height="21" rx="4" fill="white" />
-                            <path d="M23.7354 30.8233V22.1763L31.1471 26.4998L23.7354 30.8233Z" fill="#19323E" />
-                            <circle cx="26.5" cy="26.5" r="26" stroke="white" />
-                        </svg>
-                    </a>
-                </li>
-            </ul>
-        </div>
+  <div class="lg:grid grid-flow-col auto-cols-auto">
+    <div class="footer--contact">
+      <h2 class="mt-16 mb-6">Contact Us</h2>
+      <ul class="text-xl">
+        <li class="flex gap-4 my-5">
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
+            <circle opacity="0.1" cx="18" cy="18" r="18" fill="white" />
+            <path d="M18 28C17.85 28 17.7 27.9301 17.61 27.8101C17.55 27.7401 16.19 26.0001 14.81 23.4301C14 21.9201 13.35 20.43 12.88 19.01C12.29 17.22 12 15.54 12 14C12 10.69 14.69 8 18 8C21.31 8 24 10.69 24 14C24 15.54 23.7 17.22 23.12 19.01C22.65 20.43 22.01 21.9101 21.19 23.4301C19.81 26.0001 18.45 27.7401 18.39 27.8101C18.29 27.9301 18.15 28 18 28ZM18 9C15.24 9 13 11.24 13 14C13 17.25 14.46 20.66 15.69 22.95C16.59 24.63 17.5 25.96 18 26.66C18.5 25.96 19.41 24.6201 20.32 22.9301C21.54 20.6501 23 17.24 23 14C23 11.24 20.76 9 18 9Z" fill="white" />
+            <path d="M18 17C16.35 17 15 15.65 15 14C15 12.35 16.35 11 18 11C19.65 11 21 12.35 21 14C21 15.65 19.65 17 18 17ZM18 12C16.9 12 16 12.9 16 14C16 15.1 16.9 16 18 16C19.1 16 20 15.1 20 14C20 12.9 19.1 12 18 12Z" fill="white" />
+          </svg>
+          <a href="https://maps.app.goo.gl/wAtfvdJAcTVhbENc8" target="_blank" class="max-w-[21ch]">4900 North Damen Ave. Chicago, IL 60625</a>
+        </li>
+        <li class="flex gap-4 my-5">
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
+            <circle opacity="0.1" cx="18" cy="18" r="18" fill="white" />
+            <path d="M22.98 29C21.21 29 19.33 28.5 17.38 27.51C15.59 26.6 13.82 25.29 12.26 23.73C10.7 22.17 9.4 20.4 8.49 18.6C7.5 16.65 7 14.77 7 13C7 11.85 8.07 10.7399 8.53 10.3199C9.19 9.70995 10.23 9 10.99 9C11.37 9 11.81 9.25002 12.38 9.77002C12.81 10.16 13.28 10.7001 13.76 11.3101C14.05 11.6801 15.49 13.58 15.49 14.49C15.49 15.24 14.64 15.7601 13.75 16.3101C13.4 16.5201 13.05 16.74 12.79 16.95C12.51 17.17 12.47 17.2901 12.46 17.3101C13.41 19.6801 16.31 22.58 18.68 23.52C18.7 23.52 18.82 23.4699 19.04 23.1899C19.25 22.9299 19.47 22.57 19.68 22.23C20.23 21.34 20.75 20.49 21.5 20.49C22.41 20.49 24.31 21.93 24.68 22.22C25.3 22.7 25.83 23.18 26.22 23.6C26.75 24.17 26.99 24.61 26.99 24.99C26.99 25.75 26.28 26.79 25.67 27.45C25.25 27.91 24.14 28.99 22.99 28.99L22.98 29ZM10.97 10C10.7 10 9.98 10.33 9.2 11.05C8.46 11.74 7.99 12.48 7.99 13C7.99 19.73 16.25 28 22.98 28C23.49 28 24.24 27.54 24.92 26.79C25.64 26 25.97 25.28 25.98 25.01C25.95 24.82 25.42 24.08 23.98 22.97C22.74 22.02 21.74 21.51 21.48 21.5C21.46 21.5 21.35 21.55 21.12 21.84C20.92 22.09 20.71 22.43 20.51 22.77C19.95 23.68 19.42 24.54 18.65 24.54C18.53 24.54 18.4 24.52 18.29 24.47C15.67 23.42 12.56 20.3199 11.51 17.6899C11.38 17.3799 11.36 16.8799 11.98 16.3199C12.31 16.0199 12.77 15.74 13.21 15.47C13.54 15.27 13.89 15.06 14.14 14.86C14.43 14.63 14.47 14.52 14.48 14.5C14.48 14.24 13.96 13.24 13.01 12C11.9 10.56 11.16 10.04 10.97 10Z" fill="white" />
+            <path d="M26.48 18C26.2 18 25.98 17.78 25.98 17.5C25.98 13.36 22.62 10 18.48 10C18.2 10 17.98 9.78 17.98 9.5C17.98 9.22 18.2 9 18.48 9C20.75 9 22.88 9.87999 24.49 11.49C26.1 13.1 26.98 15.23 26.98 17.5C26.98 17.78 26.76 18 26.48 18Z" fill="white" />
+            <path d="M23.48 18C23.2 18 22.98 17.78 22.98 17.5C22.98 15.02 20.96 13 18.48 13C18.2 13 17.98 12.78 17.98 12.5C17.98 12.22 18.2 12 18.48 12C21.51 12 23.98 14.47 23.98 17.5C23.98 17.78 23.76 18 23.48 18Z" fill="white" />
+            <path d="M20.48 18C20.2 18 19.98 17.78 19.98 17.5C19.98 16.67 19.31 16 18.48 16C18.2 16 17.98 15.78 17.98 15.5C17.98 15.22 18.2 15 18.48 15C19.86 15 20.98 16.12 20.98 17.5C20.98 17.78 20.76 18 20.48 18Z" fill="white" />
+          </svg>
+          <a href="tel:7737847091" target="_blank">(773) 784-7091</a>
+        </li>
+        <li class="flex gap-4 my-5">
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
+            <circle opacity="0.1" cx="18" cy="18" r="18" fill="white" />
+            <path d="M25.84 10.1267C25.7 9.99675 25.49 9.96675 25.31 10.0368L6.31 18.0368C6.12 18.1168 6 18.3067 6 18.5167C6 18.7267 6.14003 18.9068 6.34003 18.9768L12 20.8667V26.5068C12 26.7168 12.13 26.9068 12.33 26.9768C12.38 26.9968 12.44 27.0068 12.5 27.0068C12.65 27.0068 12.79 26.9367 12.89 26.8267L15.65 23.4267L20.15 27.8667C20.24 27.9567 20.37 28.0068 20.5 28.0068C20.54 28.0068 20.58 28.0067 20.62 27.9867C20.79 27.9467 20.92 27.8167 20.97 27.6467L25.97 10.6467C26.03 10.4667 25.97 10.2668 25.82 10.1368L25.84 10.1267ZM22.14 12.4567L12.42 19.9367L7.92999 18.4367L22.14 12.4567ZM13.01 20.7467L22.42 13.5068L15.23 22.3567L13.01 25.0867V20.7367V20.7467ZM20.27 26.5567L16.3 22.6368L24.33 12.7467L20.27 26.5567Z" fill="white" />
+          </svg>
+          <a href="mailto:office@ravenswoodcov.org" target="_blank">office@ravenswoodcov.org</a>
+        </li>
+      </ul>
     </div>
-    <p class="text-center text-sm mt-12 max-w-[40ch] md:max-w-none mx-auto">
-        © Ravenswood Evangelical Covenant Church <? echo date('Y') ?>. All rights reserved.
-    </p>
+    <div class="footer--links">
+      <h2 class="mt-16 mb-6">
+        Links
+      </h2>
+      <ul class="text-xl">
+        <li class="my-4">
+          <a href="<?php echo site_url('/about-us'); ?>">What we believe</a>
+        </li>
+        <li class="my-4">
+          <a href="https://calendar.google.com/calendar/u/0/embed?showTitle=0&showCalendars=0&height=600&wkst=1&bgcolor=%2333cc00&src=en.christian%23holiday@group.v.calendar.google.com&color=%23125A12&src=ravenscov.org_bmluo3ktbg99jdv6kgi57ptnq4@group.calendar.google.com&color=%232952A3&src=en.usa%23holiday@group.v.calendar.google.com&color=%23182C57&ctz=America/Chicago" target="_blank">Church calendar</a>
+        </li>
+        <li class="my-4">
+          <a href="https://covchurch.org/" target="_blank">Our denomination</a>
+        </li>
+        <li class="my-4">
+          <a href="https://centralconf.org/" target="_blank">ECC Central Conference</a>
+        </li>
+        <li>
+        <a href="<? echo site_url('/directory') ?>">Directory</a>
+        </li>
+      </ul>
+    </div>
+    <div class="footer--socials">
+      <h2 class="mt-16 mb-6">Social media</h2>
+      <ul class="flex gap-6">
+        <li>
+          <a href="https://www.facebook.com/ravenscov/" target="_blank">
+            <img src="<?php echo get_theme_file_uri('/images/facebook.svg') ?>" class="" alt="facebook">
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/channel/UCGUlqyTaZxBPs-oJGn2nxvQ" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="53" height="53" viewBox="0 0 53 53" fill="none">
+              <rect x="12" y="16" width="29.6471" height="21" rx="4" fill="white" />
+              <path d="M23.7354 30.8233V22.1763L31.1471 26.4998L23.7354 30.8233Z" fill="#19323E" />
+              <circle cx="26.5" cy="26.5" r="26" stroke="white" />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <p class="text-center text-sm mt-12 max-w-[40ch] md:max-w-none mx-auto">
+    © Ravenswood Evangelical Covenant Church <? echo date('Y') ?>. All rights reserved.
+  </p>
 </footer>
 
 <?php wp_footer(); ?>
